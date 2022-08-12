a971b0baf4f1ff23d24043d9909aba2e0d03d02e
Krissttina <krismetodieva123@gmail.com>
2022-08-12 15:00:23 +0300

    new file

---
+++ script.js
@@ -0,0 +1,16 @@
+/*var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+*/
+const addBtn = document.createElement('div .add-btn');
+
+addBtn.addEventListener('click', () => AddButton());
+
+function AddButton() {
+    addBtn;
+}
